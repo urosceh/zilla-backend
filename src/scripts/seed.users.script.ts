c3ae0b759ef50e11b6ed71b21865fe9e8141e665
urosceh <urosvceh@gmail.com>
2025-10-20 21:01:27 +0200

    add seed users script

---
+++ src/scripts/seed.users.script.ts
@@ -0,0 +1,130 @@
+import UserModel from "../database/models/user.model";
+
+// Get command line arguments
+const args = process.argv.slice(2);
+const amountOfUsers = parseInt(args[0]);
+const domainName = args[1];
+
+if (!amountOfUsers || !domainName || amountOfUsers > 500 || amountOfUsers < 1) {
+  console.error("Usage: ts-node seed.users.script.ts <amountOfUsers> <domainName>");
+  console.error("Amount of users must be between 1 and 500");
+  process.exit(1);
+}
+
+// Pool of 25 first names and 25 last names
+const firstNames = [
+  "Alex",
+  "Ben",
+  "Cole",
+  "Dan",
+  "Eva",
+  "Fox",
+  "Gus",
+  "Hal",
+  "Ivy",
+  "Jay",
+  "Kim",
+  "Leo",
+  "Max",
+  "Nia",
+  "Oak",
+  "Pip",
+  "Rex",
+  "Sam",
+  "Tia",
+  "Uma",
+  "Val",
+  "Wade",
+  "Xara",
+  "Yve",
+  "Zoe",
+];
+
+const lastNames = [
+  "Ash",
+  "Bell",
+  "Cox",
+  "Day",
+  "Ely",
+  "Fox",
+  "Gray",
+  "Hill",
+  "Ice",
+  "Joy",
+  "King",
+  "Lee",
+  "May",
+  "Nash",
+  "Oak",
+  "Pike",
+  "Ross",
+  "Sun",
+  "Tay",
+  "Uhl",
+  "Vale",
+  "West",
+  "Xu",
+  "York",
+  "Zane",
+];
+
+// Function to generate random alphanumeric password (8 characters)
+function generateRandomPassword(): string {
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let password = "";
+  for (let i = 0; i < 8; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+}
+
+// Function to get name combination by index (sequential, not random)
+function getNameByIndex(index: number): {firstName: string; lastName: string} {
+  const firstNameIndex = (index - 1) % firstNames.length;
+  const lastNameIndex = Math.floor((index - 1) / firstNames.length) % lastNames.length;
+  return {
+    firstName: firstNames[firstNameIndex],
+    lastName: lastNames[lastNameIndex],
+  };
+}
+
+// Function to generate unique email
+function generateEmail(firstName: string, lastName: string, index: number): string {
+  return `${firstName.toLowerCase()}.${lastName.toLowerCase()}@${domainName}`;
+}
+
+(async () => {
+  try {
+    const users = [];
+
+    for (let i = 1; i <= amountOfUsers; i++) {
+      const {firstName, lastName} = getNameByIndex(i);
+      const email = generateEmail(firstName, lastName, i);
+      const password = generateRandomPassword();
+
+      const user = await UserModel.create(
+        {
+          email,
+          password,
+          firstName,
+          lastName,
+        },
+        {
+          logging: false,
+        }
+      );
+
+      users.push({email, password});
+    }
+
+    // Log only the email and password pairs
+    users.forEach((user) => {
+      console.log(`${user.email},${user.password}`);
+    });
+  } catch (error) {
+    console.error("Error creating users:", error);
+    process.exit(1);
+  }
+
+  process.exit(0);
+})();
