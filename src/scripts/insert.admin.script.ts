c7309889bf358e16b7c283d2f1328b14bb9e60d3
urosceh <urosvceh@gmail.com>
2025-10-20 20:23:48 +0200

    add insert admin script

---
+++ src/scripts/insert.admin.script.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import AdminUserModel from "../database/models/admin.user.model";
+import UserModel from "../database/models/user.model";
+
+dotenv.config();
+
+// Get command line arguments
+const args = process.argv.slice(2);
+const adminEmail = args[0];
+const adminPassword = args[1];
+
+if (!adminEmail || !adminPassword) {
+  console.error("ADMIN_EMAL and ADMIN_PASSWORD not found in .env");
+  process.exit(1);
+}
+
+console.log(`Admin email: ${adminEmail}`);
+
+(async () => {
+  try {
+    const adminUser = await UserModel.create(
+      {
+        email: adminEmail,
+        password: adminPassword,
+        firstName: "Admin",
+        lastName: "Admin",
+      },
+      {
+        returning: true,
+      }
+    );
+
+    await AdminUserModel.create({userId: adminUser.userId});
+
+    console.log(`Admin user created successfully! Email: ${adminEmail}`);
+  } catch (error) {
+    console.error("Error creating admin user:", error);
+    process.exit(1);
+  }
+
+  process.exit(0);
+})();
