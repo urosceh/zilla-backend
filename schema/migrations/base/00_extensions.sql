e9cf636b9895579ac348556a6f280863e8b94184
urosceh <urosvceh@gmail.com>
2025-10-21 23:42:41 +0200

    commit creating extension with public schema

--- schema/migrations/base/00_extensions.sql
+++ schema/migrations/base/00_extensions.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 -- changeset uros:add-uuid-extension
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 -- changeset uros:create-pgcrypto-extension
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
